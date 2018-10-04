7b589c962aa22baeec58fdf956fcfb334d609d8e
scottyd_14 <scottyd_14@hotmail.com>
2018-10-04 16:34:54 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,8 +15,8 @@ def batch_badge_creator(array)
 end 
 
 def assign_rooms(array)
-  array.each do |name|
-    "Hello, #{name}! You'll be assigned to room "
+  array.each_with_index do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index +1} "
   end
   
 end
